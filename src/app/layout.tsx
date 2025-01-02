0c5ed43f65a285085f2f53f93fab1be702cda37f
Jared Rigby <talk@jaredrigby.co.uk>
2025-01-02 21:05:24 +0000

    Added Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react"
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -35,6 +36,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <SpeedInsights/>
+        <Analytics />
       </body>
     </html>
   );
