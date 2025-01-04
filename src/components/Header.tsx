3a46c3ec7c0cdcf123b684687f703b5be8ab6e7e
Jared Rigby <talk@jaredrigby.co.uk>
2025-01-04 20:51:24 +0000

    Added a link to the homepage in the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,9 @@ export default function Header() {
     <header className="bg-zinc-900 text-white font-mono">
       <div className="flex flex-col md:flex-row items-start gap-8 justify-between md:items-center p-4 md:px-16 md:py-8 m-auto max-w-screen-2xl">
         <div>
-          <h1 className="text-xl md:text-3xl">Daily Game Dev 2025</h1>
+          <h1 className="text-xl md:text-3xl">
+            <Link className="decoration-white/70 underline decoration-dotted" href="https://dailydev.jaredrigby.co.uk">Daily Game Dev 2025</Link>
+          </h1>
           <h2 className="text-lg md:text-xl text-white/70">
             <Link className="decoration-white/70 underline decoration-dotted" href="https://jaredrigby.co.uk">By Jared Rigby</Link>
           </h2>
